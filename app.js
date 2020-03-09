94fb4ab20b766e3c62223cd1958067a7e00995e3
Austin Vieth <avieth@payality.com>
2020-03-09 12:39:52 -0700

    Add GoogleOAuth and button styling

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const bcrypt = require('bcrypt');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
@@ -38,21 +40,63 @@ const userSchema = new mongoose.Schema({
   username:
   {
     type: String,
-  }
+  },
+  googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+///////////////////////////////Google OAuth///////////////////////////////////
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+    // userProfileURL: "https://www.googleapis.com/oath2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, done) {
+      console.log(profile);
+
+       User.findOrCreate({ googleId: profile.id }, function (err, user) {
+         return done(err, user);
+       });
+  }
+));
+
+
+
+
+
+///////////////////////////////////Routing//////////////////////////////
+
 
 app.get("/", function(req, res){
   res.render("home");
 });
+
+app.get('/auth/google/secrets',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/secrets');
+  });
+
+app.get("/auth/google",
+  passport.authenticate("google", { scope: ["https://www.googleapis.com/auth/plus.login"] }));
 
 
 app.route("/login")
